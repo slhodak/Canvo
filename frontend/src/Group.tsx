164ead4ba8c84fe023b4b1d97c3bc964f7b88f58
Sam Hodak <slhodak@gmail.com>
2025-01-15 17:31:49 -0800

    Layout group stuff

--- frontend/src/Group.tsx
+++ frontend/src/Group.tsx
@@ -1,25 +1,40 @@
-import { useState, useEffect } from 'react';
+import { useState, useEffect, useCallback } from 'react';
+import './Group.css';
 import { BlockModel, GroupModel } from '@wb/shared-types';
 import { SERVER_URL } from './constants';
 
 export const Group = ({ group }: { group: GroupModel }) => {
   const [blocks, setBlocks] = useState<BlockModel[]>([]);
 
-  useEffect(() => {
-    async function fetchBlocks() {
-      const response = await fetch(`${SERVER_URL}/api/get_blocks_for_group/${group._id}`, {
-        credentials: 'include',
-      });
-      const data = await response.json();
-      setBlocks(data.blocks);
-    }
+  const fetchBlocks = useCallback(async () => {
+    const response = await fetch(`${SERVER_URL}/api/get_blocks_for_group/${group._id}`, {
+      credentials: 'include',
+    });
+    const data = await response.json();
+    setBlocks(data.blocks);
+  }, [group._id]);
 
+  const addBlock = async () => {
+    const response = await fetch(`${SERVER_URL}/api/new_block`, {
+      method: 'POST',
+      credentials: 'include',
+    });
+    const data = await response.json();
+    if (data.status == 'success') {
+      fetchBlocks();
+    }
+  }
+
+  useEffect(() => {
     fetchBlocks();
-  }, [group]);
+  }, [fetchBlocks]);
 
   return <div className="group-container">
-    <div className="group-label">Group Name: {group.label ?? 'unknown'}</div>
-    <div className="group-blocks">
+    <div className="group-header-container">
+      <div className="group-label">Group Name: {group.label ?? 'unknown'}</div>
+      <button className="add-block-button" onClick={addBlock}>Add Block</button>
+    </div>
+    <div className="group-blocks-container">
       {blocks.map((block) => (
         <div className="block-container">{block.label}</div>
       ))}
