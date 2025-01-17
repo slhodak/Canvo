fe25b937d325ec51d49bcab074a7b2947ad06545
Sam Hodak <slhodak@gmail.com>
2025-01-16 20:04:55 -0800

    replace submodule with regular sub folder

---
+++ shared/types/src/types.ts
@@ -0,0 +1,53 @@
+export interface UserModel {
+  id: number;
+  _id: string;
+  email: string;
+  created_at: Date;
+  updated_at: Date;
+}
+
+export interface SessionModel {
+  id: number;
+  session_token: string;
+  user_email: string;
+  session_start: Date;
+  session_expiration: Date;
+}
+
+export interface GroupModel {
+  id: number;
+  _id: string;
+  author_id: string;
+  label: string;
+  created_at: Date;
+  updated_at: Date;
+}
+
+export interface BlockModel {
+  id: number;
+  _id: string;
+  group_id: string;
+  author_id: string;
+  label: string;
+  content: string;
+  created_at: Date;
+  updated_at: Date;
+}
+
+export interface TransformationModel {
+  id: number;
+  _id: string;
+  group_id: string;
+  author_id: string;
+  input_block_id: string;
+  label: string;
+  prompt: string;
+  created_at: Date;
+  updated_at: Date;
+}
+
+export interface TransformationOutputModel {
+  id: number;
+  transformation_id: string;
+  output_block_id: string;
+}
