53c23f5a6f55cdeaac1e5594ed337bc8c87b8aef
Sam Hodak <slhodak@gmail.com>
2025-02-12 17:33:54 -0800

    fix provision server script

--- scripts/provision_server.sh
+++ scripts/provision_server.sh
@@ -3,9 +3,11 @@
 # For an AL2023 Server
 
 # Node.js & Yarn
+echo "Installing Node.js & Yarn..."
 sudo dnf install nodejs yarn -y
 
 # Python & Poetry
+echo "Installing Python & Poetry..."
 sudo dnf install python3.12 python3.12-pip -y
 python3.12 -m pip install --user pipx
 python3.12 -m ensurepath
@@ -13,15 +15,18 @@ source ~/.bashrc
 pipx install poetry
 
 # FastAPI
+echo "Installing uvicorn..."
 sudo dnf install uvicorn -y
 
 # PostgreSQL
-sudo dnf install postgresql16 postgresl16-server -y
+echo "Installing PostgreSQL..."
+sudo dnf install postgresql16 postgresql16-server -y
 sudo postgresql-setup --initdb
 sudo systemctl start postgresql-16
 sudo systemctl enable postgresql-16
 
 # Nginx
+echo "Installing Nginx..."
 sudo dnf install nginx -y
 sudo systemctl start nginx
 sudo systemctl enable nginx
