ca781a248c6630d279aeb844287faddb7b3da916
Sam Hodak <slhodak@gmail.com>
2025-02-24 11:04:40 -0800

    styling csv table

--- frontend/src/OutputView.tsx
+++ frontend/src/OutputView.tsx
@@ -71,7 +71,26 @@ const OutputView = ({ outputState }: OutputViewProps) => {
         ) : selectedStateType === IOStateType.StringArray ? (
           <div className="output-view-string-array">{renderValue()}</div>
         ) : selectedStateType === IOStateType.Table ? (
-          <div className="output-view-table">{renderValue()}</div>
+          <div className="output-view-table">
+            <div className="output-view-table-header">
+              {outputState.tableValue?.[0].map((header, index) => (
+                <div key={`table-header-${index}`} className="output-view-table-header-item">
+                  {header}
+                </div>
+              ))}
+            </div>
+            <div className="output-view-table-body">
+              {outputState.tableValue?.slice(1).map((row, index) => (
+                <div key={`table-row-${index}`} className="output-view-table-row">
+                  {row.map((cell, cellIndex) => (
+                    <div key={`table-cell-${cellIndex}`} className="output-view-table-cell">
+                      {cell}
+                    </div>
+                  ))}
+                </div>
+              ))}
+            </div>
+          </div>
         ) : (
           <div className="output-view-text">None</div>
         )}
