8cd73cc07e23f27968df9a05ab21047c8d6262a7
Sam Hodak <slhodak@gmail.com>
2025-02-26 09:45:30 -0800

    fix empty table view crash

--- frontend/src/OutputView.tsx
+++ frontend/src/OutputView.tsx
@@ -56,10 +56,10 @@ const OutputView = ({ outputState }: OutputViewProps) => {
           </div>
         ) : selectedStateType === IOStateType.Table ? (
           <TableView outputState={outputState} />
-        ): (
-            <div className = "output-view-text">None</div>
+        ) : (
+          <div className="output-view-text">None</div>
         )}
-    </div>
+      </div>
     </div >
   );
 };
@@ -69,24 +69,30 @@ export default OutputView;
 const TableView = ({ outputState }: OutputViewProps) => {
   return (
     <div className="output-view-table">
-      <div className="output-view-table-header">
-        {outputState.tableValue?.[0].map((header, index) => (
-          <div key={`table-header-${index}`} className="output-view-table-header-item">
-            {header}
-          </div>
-        ))}
-      </div>
-      <div className="output-view-table-body">
-        {outputState.tableValue?.slice(1).map((row, index) => (
-          <div key={`table-row-${index}`} className="output-view-table-row">
-            {row.map((cell, cellIndex) => (
-              <div key={`table-cell-${cellIndex}`} className="output-view-table-cell">
-                {cell}
+      {outputState.tableValue?.[0] ? (
+        <>
+          <div className="output-view-table-header">
+            {outputState.tableValue?.[0]?.map((header, index) => (
+              <div key={`table-header-${index}`} className="output-view-table-header-item">
+                {header}
               </div>
             ))}
           </div>
-        ))}
-      </div>
-    </div>
-  );
+          <div className="output-view-table-body">
+            {outputState.tableValue?.slice(1).map((row, index) => (
+              <div key={`table-row-${index}`} className="output-view-table-row">
+                {row.map((cell, cellIndex) => (
+                  <div key={`table-cell-${cellIndex}`} className="output-view-table-cell">
+                    {cell}
+                  </div>
+                ))}
+              </div>
+            ))}
+          </div>
+        </>
+      ) : (
+        <div className="output-view-table-header">No data</div>
+      )}
+        </div>
+      );
 }
