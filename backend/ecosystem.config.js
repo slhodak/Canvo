d8236151d97a3cd1b09d9705a068bd9650a6acc1
Sam Hodak <slhodak@gmail.com>
2025-03-05 15:53:25 -0800

    fix app entry point location in script

--- backend/ecosystem.config.js
+++ backend/ecosystem.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   apps: [{
     name: "canvo-server",
-    script: "./dist/app.js",
+    script: "./dist/backend/src/app.js",
     env: {
       "NODE_ENV": "production",
       "FRONTEND_PATH": "/home/ec2-user/canvo/frontend/dist"
