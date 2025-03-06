7c8fb0d38109e3354c1447e929fe29b463f28da8
Sam Hodak <slhodak@gmail.com>
2025-03-05 17:39:08 -0800

    messy

--- frontend/src/constants.ts
+++ frontend/src/constants.ts
@@ -1,4 +1,8 @@
-export const SERVER_URL = import.meta.env.VITE_SERVER_DOMAIN;
-export const AI_SERVICE_URL = import.meta.env.VITE_AI_SERVICE_DOMAIN;
-export const STYTCH_PUBLIC_TOKEN = import.meta.env.VITE_STYTCH_PUBLIC_TOKEN
-export const APP_DOMAIN = import.meta.env.PROD ? "canvo.app" : "";
+import dotenv from 'dotenv';
+
+dotenv.config({ path: `.env.${process.env.ENV}` });
+
+export const SERVER_URL = process.env.VITE_SERVER_DOMAIN;
+export const AI_SERVICE_URL = process.env.VITE_AI_SERVICE_DOMAIN;
+export const STYTCH_PUBLIC_TOKEN = process.env.VITE_STYTCH_PUBLIC_TOKEN
+export const APP_DOMAIN = process.env.PROD ? "canvo.app" : "";
