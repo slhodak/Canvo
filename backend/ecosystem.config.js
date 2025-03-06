5540a92150440968bd2a8c008672bed096755220
Sam Hodak <slhodak@gmail.com>
2025-03-05 21:04:47 -0800

    fix some final deploy bugs

--- backend/ecosystem.config.js
+++ backend/ecosystem.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   apps: [{
     name: "canvo-server",
-    script: "./dist/backend/src/app.js",
+    script: "./dist/app.js",
     env: {
       "NODE_ENV": "production",
       "FRONTEND_PATH": "/home/ec2-user/canvo/frontend/dist"
