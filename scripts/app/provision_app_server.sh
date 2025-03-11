107ce5a2e42d6338f63672c5a5d6105fb7822495
Sam Hodak <slhodak@gmail.com>
2025-03-10 18:03:18 -0700

    Configure nginx connections via nginx reverse proxy

--- scripts/app/provision_app_server.sh
+++ scripts/app/provision_app_server.sh
@@ -6,19 +6,19 @@ if [ "$EUID" -ne 0 ]; then
     exit 1
 fi
 
-# These script locations are flattened into the root directory when they're copied to the server
-./install_app_dependencies.sh
-./configure_nginx.sh
+# Script locations will reflect folder the structure they were compressed from
+./scripts/app/install_app_dependencies.sh
+./scripts/app/configure_nginx.sh
 
 # Install the canvo-ai service
-sudo cp canvo-ai.service /etc/systemd/system/canvo-ai.service
+sudo cp server-config/canvo-ai.service /etc/systemd/system/canvo-ai.service
 sudo mkdir -p /var/log/canvo-ai
 sudo chown ec2-user:ec2-user /var/log/canvo-ai
 sudo systemctl enable canvo-ai
 sudo systemctl start canvo-ai
 
 # Install the canvo-server service
-sudo cp canvo-server.service /etc/systemd/system/canvo-server.service
+sudo cp server-config/canvo-server.service /etc/systemd/system/canvo-server.service
 sudo mkdir -p /var/log/canvo-server
 sudo chown ec2-user:ec2-user /var/log/canvo-server
 sudo systemctl enable canvo-server
